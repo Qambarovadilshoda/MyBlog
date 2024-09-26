df4456c82df4d562ff5ae2ef962649f0e6ebaa32
Dilshoda <qambarovadilshoda867@gmail.com>
2024-09-26 14:20:00 +0500

    Updated

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,24 +4,9 @@
 
 @section('content')
 
-<!-- Page Header Start -->
-<div class="container-fluid bg-primary py-5 mb-5">
-    <div class="container py-5">
-        <div class="row align-items-center py-4">
-            <div class="col-md-6 text-center text-md-left">
-                <h1 class="display-4 mb-4 mb-md-0 text-secondary text-uppercase">Blog</h1>
-            </div>
-            <div class="col-md-6 text-center text-md-right">
-                <div class="d-inline-flex align-items-center">
-                    <a class="btn btn-sm btn-outline-light" href="">Home</a>
-                    <i class="fas fa-angle-double-right text-light mx-2"></i>
-                    <a class="btn btn-sm btn-outline-light disabled" href="">Blog</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Page Header End -->
+<x-page-header>
+    Blog
+</x-page-header>
 
 
 <!-- Blog Start -->
