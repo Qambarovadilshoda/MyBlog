a6624ec27d62ce024288691b3669752b49efdb3a
Dilshoda <qambarovadilshoda867@gmail.com>
2024-10-02 06:16:15 +0500

    Created new migration

--- database/migrations/2024_10_02_010633_create_tags_table.php
+++ database/migrations/2024_10_02_010633_create_tags_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post_tag', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
-            $table->string('name');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('post_tag');
+        Schema::dropIfExists('tags');
     }
 };
