bac616481f5b27b3ca5e21d99b4879f95431986e
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-14 21:21:56 +0500

    I added AuthController for manage register and login user's

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SendSmsToMail;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\StoreAuthRequest;
 use App\Http\Requests\StoreUserLoginRequest;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -20,11 +22,12 @@ class AuthController extends Controller
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
+        $user->verification_token = uniqid();
         $user->password = bcrypt($request->password);
         $user->save();
 
-        Auth::login($user);
-        return redirect()->route('home');
+        Mail::to($user->email)->send(new SendSmsToMail($user));
+        return redirect()->route('login');
     }
     public function loginForm()
     {
@@ -34,6 +37,9 @@ class AuthController extends Controller
     {
         $user = User::where('email', $request->email)->first();
         if ($user && Hash::check($request->password, $user->password)) {
+            if($user->email_verified_at == null){
+                abort(403);
+            }
             Auth::attempt(['email' => $request->email, 'password' => $request->password]);
             return redirect()->route('home');
         }
@@ -43,5 +49,14 @@ class AuthController extends Controller
     {
         Auth::logout();
         return redirect()->route('home');
+    }
+    public function verify(Request $request){
+        $user = User::where('verification_token', $request->token)->first();
+        if(!$user){
+            abort(404);
+        }
+        $user->email_verified_at = now();
+        $user->save();
+        return redirect()->route('login');
     }
 }
